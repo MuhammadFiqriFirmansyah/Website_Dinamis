2f4413ce52cee38489aa4de0643d2fb999f4a4aa
Fiqri <mfiqrifirmansyah.92@gmail.com>
2024-11-04 23:26:58 +0700

    WebStatis

---
+++ includes/home.php
@@ -0,0 +1,21 @@
+    <!-- Hero Section -->
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1 class="display-4">Hai, Selamat Datang di Web Saya</h1>
+            <p class="lead">Let's be Friends</p>
+            <a href="?page=about" class="btn btn-info btn-lg">About Me</a>
+        </div>
+    </section>
+
+    <!-- Tentang Saya -->
+    <section id="tentang" class="container py-5">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="assets/images/Profile.jpg" alt="Profile" class="img-fluid rounded">
+            </div>
+            <div class="col-md-6">
+                <h2>Tentang Saya</h2>
+                <p>Saya Adalah Mahasiswa di Universitas Pelita Bangsa.</p>
+            </div>
+        </div>
+    </section>
